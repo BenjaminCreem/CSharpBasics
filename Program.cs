59464b327c4e8b1c22b7a208b31080e0b0b1c355
BenjaminCreem <benjamincreem@gmail.com>
2017-02-21 23:09:06 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace FirstConsoleProject
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			//BASIC CALCULATOR
+			//Reading in a number
+			//Console.Write("Enter a number\n");
+			//int num01;
+			//num01 = Convert.ToInt32(Console.ReadLine());
+			//Console.Write("Enter another number\n");
+			//int num02 = Convert.ToInt32(Console.ReadLine());
+			//Printing num1 + num 2
+			//Console.Write("\nThe answer is: " + (num01 + num02));
+
+
+
+			//IF STATEMENTS
+			//Console.Write("How old are you\n");
+			//int age = Convert.ToInt32(Console.ReadLine());
+			//if (age >= 18)
+			//{
+			//	Console.WriteLine("You are an adult");
+			//}
+			//else
+			//{
+			//	Console.WriteLine("You are not an adult");
+			//}
+
+
+			//FOR LOOPS
+			//for (int i = 0; i < 10; i++)
+			//{
+			//	Console.WriteLine("Example of a For Loop");
+			//}
+
+
+			//WHILE LOOPS
+			//int i = 1;
+			//while (i != 10)
+			//{
+			//	Console.WriteLine(i);
+			//	i++;
+			//}
+
+			//METHOD CALLS
+			//thisIsAMethod();
+
+
+			//ARRAYS
+			//int[] arrayExample = new int[10];
+			//for (int i = 0; i < arrayExample.Length; i++){
+			//	arrayExample[i] = i;
+			//}
+			//for (int i = 0; i < arrayExample.Length; i++)
+			//{
+			//	Console.WriteLine(arrayExample[i]);
+			//}
+
+
+		}
+		public static void thisIsAMethod()
+		{
+			Console.WriteLine("This is a method");
+		}
+	}
+}
